2d14b82ed11b787e79b115666aa52f11656affd9
Chananyu Pumpradub <chananyu2539@hotmail.com>
2017-03-16 12:35:12 +0700

    test

---
+++ src/LeftSidenav.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <div class="left-sidenav">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title">Homework 2</h2>
+    </md-toolbar>
+    <md-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+        <md-list>
+          <md-list-item>
+            <router-link :to="{ name: 'Posts.index' }">
+              <span>All post</span>
+            </router-link>
+          </md-list-item>
+
+          <md-list-item>
+            <router-link :to="{ name: 'Posts.new' }">
+              <span>New post</span>
+            </router-link>
+          </md-list-item>
+
+          <md-list-item>
+            <md-button class="md-raised md-primary" @click.native="logOut">
+              <span>Log out</span>
+            </md-button>
+          </md-list-item>
+        </md-list>
+      </md-toolbar>
+    </md-sidenav>
+  </div>
+</template>
+
+<script>
+import usersAPI from '@/api/users.js'
+export default {
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    logOut () {
+      usersAPI.logout(function () {
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,18 +3,26 @@ import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+import store from './store'
 
 Vue.use(VueMaterial)
 
 import Axios from 'axios'
 Vue.$http = Axios
+Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+Axios.defaults.headers.common.Accept = 'application/json'
+Axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
+import UsersApi from '@/api/users.js'
+
+UsersApi.checkLoggedIn()
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
